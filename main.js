695105605d751bed7bbc5c434706bca0e92ad444
SaiKrishna Emmadishetty <S538340@nwmissouri.edu>
2020-02-17 14:38:40 -0600

    all files added

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+    function calculate(){
+        let total=parseInt(document.getElementById("total").value);
+        console.log(typeof total);
+        let rate=parseInt(document.getElementById("rate").value);
+        console.log(typeof rate);
+        let grand_total=document.getElementById("grand_total");
+    
+        grand_total.innerHTML = ((total*(rate/100)) + 5.50 + total);
+        Swal.fire({
+            title: "<i>Practice App</i>", 
+            html: "Grand Total is calculated ",  
+            confirmButtonText: "OK", 
+          }); 
+    
+    }
+
